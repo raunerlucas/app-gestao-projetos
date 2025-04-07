0ac1b760745be6f8af20535418af74fcb061730a
raunerlucas <raunerlucas@gmail.com>
2025-04-07 15:48:53 -0300

    feat: exp entity

--- Backend/meusFuncionarios/src/main/java/com/meusColaboradores/model/Funcionario.java
+++ Backend/meusFuncionarios/src/main/java/com/meusColaboradores/model/Funcionario.java
@@ -1,4 +1,16 @@
 package com.meusColaboradores.model;
 
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity
 public class Funcionario {
+    // TODO: Implementar atribustos de Funcionario
+
+    @Id
+    private Long id;
+
 }
