c7da03e54209d1823f03c00c0fa4412892d7ff08
raunerlucas <raunerlucas@gmail.com>
2025-04-07 16:04:20 -0300

    feat: exp jpa

--- Backend/meusFuncionarios/src/main/java/com/meusColaboradores/repository/FuncionarioRepository.java
+++ Backend/meusFuncionarios/src/main/java/com/meusColaboradores/repository/FuncionarioRepository.java
@@ -6,4 +6,6 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface FuncionarioRepository extends JpaRepository<Funcionario, Long> {
+    // Aqui você pode adicionar métodos personalizados, se necessário
+    // Exemplo: List<Funcionario> findByNome(String nome);
 }
