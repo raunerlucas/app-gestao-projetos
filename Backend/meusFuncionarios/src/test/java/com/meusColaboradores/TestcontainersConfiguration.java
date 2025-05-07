a741f2f1623663061a820ef6ebec7fce1ae05a27
Rauner Lucas Amaral <95373893+raunerlucas@users.noreply.github.com>
2025-05-07 14:51:17 -0300

    Delete Backend/meusFuncionarios directory

--- Backend/meusFuncionarios/src/test/java/com/meusColaboradores/TestcontainersConfiguration.java
+++
@@ -1,8 +0,0 @@
-package com.meusColaboradores;
-
-import org.springframework.boot.test.context.TestConfiguration;
-
-@TestConfiguration(proxyBeanMethods = false)
-class TestcontainersConfiguration {
-
-}
