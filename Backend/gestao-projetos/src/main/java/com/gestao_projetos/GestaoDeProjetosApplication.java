beaf1fb106141a5c2d166ce26628f42b7c83671f
Rauner Lucas Amaral <95373893+raunerlucas@users.noreply.github.com>
2025-05-07 14:51:17 -0300

    feat: backend

--- Backend/gestao-projetos/src/main/java/com/gestao_projetos/GestaoDeProjetosApplication.java
+++ Backend/gestao-projetos/src/main/java/com/gestao_projetos/GestaoDeProjetosApplication.java
@@ -1,12 +1,13 @@
-package com.meusColaboradores;
+package com.gestao_projetos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MeusFuncionariosApplication {
+public class GestaoDeProjetosApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MeusFuncionariosApplication.class, args);
+		SpringApplication.run(GestaoDeProjetosApplication.class, args);
 	}
+
 }
---
+++ Backend/gestao-projetos/src/test/java/com/gestao_projetos/GestaoDeProjetosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gestao_projetos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoDeProjetosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
