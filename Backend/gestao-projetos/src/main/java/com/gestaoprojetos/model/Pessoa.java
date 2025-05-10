fb08fab9ca1f89647e857d71a79eea77b56dc67a
Aline Reis <98558289+Aline-Reis@users.noreply.github.com>
2025-05-10 18:22:08 -0300

    Fit: Implementação parcial pessoa.java
    Signed-off-by: Aline Reis <98558289+Aline-Reis@users.noreply.github.com>

--- Backend/gestao-projetos/src/main/java/com/gestaoprojetos/model/Pessoa.java
+++ Backend/gestao-projetos/src/main/java/com/gestaoprojetos/model/Pessoa.java
@@ -1,4 +1,15 @@
 package com.gestaoprojetos.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Data;
+@Entity
+@Data
 public abstract class Pessoa {
+    @Id
+    private Long id;
+    private String nome;
+    private String cpf;
+    private String telefone;
+    private String email;
 }
