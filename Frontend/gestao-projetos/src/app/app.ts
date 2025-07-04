eb6236f0a6ef1d5355a7d7f5d5352c1a5026cbce
raunerlucas <raunerlucas@gmail.com>
2025-07-04 17:20:40 -0300

    Feat: Add Home and Footer components with corresponding templates and styles

--- Frontend/gestao-projetos/src/app/app.ts
+++ Frontend/gestao-projetos/src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {Home} from './pages/home/home';
+import {Footer} from './components/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Home, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
