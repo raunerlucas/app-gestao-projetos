11b0f8a565655afdcec49fae5872b4a75b5791c2
raunerlucas <raunerlucas@gmail.com>
2025-07-17 17:13:19 -0300

    Feat: Implement Footer component with layout, styles, and logo integration

--- Frontend/gestao-projetos/src/app/shared/footer/footer.ts
+++ Frontend/gestao-projetos/src/app/shared/footer/footer.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import {Logo} from '../logo/logo';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  imports: [
+    Logo
+  ],
   templateUrl: './footer.html',
   styleUrl: './footer.css'
 })
