648d39218ba15428a9b4f2a1e22ae1a69258b5c1
raunerlucas <raunerlucas@gmail.com>
2025-07-23 19:34:25 -0300

    Feat: Update Navbar component to conditionally render login and register buttons

--- Frontend/gestao-projetos/src/app/shared/navbar/navbar.ts
+++ Frontend/gestao-projetos/src/app/shared/navbar/navbar.ts
@@ -1,20 +1,23 @@
 import {Component} from '@angular/core';
 import {Logo} from '../logo/logo';
+import {NgIf} from '@angular/common';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   imports: [
-    Logo
+    Logo,
+    NgIf,
+    RouterLink
   ],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css'
 })
 export class Navbar {
+  btnLogin: Boolean = true;
+  btnRegister: Boolean = true;
+
   toggleTheme() {
     // implementar troca de tema claro/escuro
-  }
-
-  openLoginPage() {
-
   }
 }
