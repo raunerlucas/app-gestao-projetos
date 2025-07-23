092afee48a824c3520b4fed92ffb4ee13f2e33ed
raunerlucas <raunerlucas@gmail.com>
2025-07-23 19:34:53 -0300

    Feat: Update routing to include Home and Register components; replace app-home with router-outlet

--- Frontend/gestao-projetos/src/app/app.routes.ts
+++ Frontend/gestao-projetos/src/app/app.routes.ts
@@ -1,9 +1,16 @@
 import {Routes} from '@angular/router';
 import {Login} from './pages/login/login';
 import {Dashboard} from './pages/dashboard/dashboard';
+import {Home} from './pages/home/home';
+import {Register} from './pages/register/register';
 
 export const routes: Routes = [
-  { path: 'login', component: Login },
-  { path: 'dashboard', component: Dashboard },
-  { path: '', redirectTo: '/login', pathMatch: 'full' }
+// Define as rotas da aplicação
+  {path: 'home', component: Home},
+  {path: 'login', component: Login},
+  {path: 'register', component: Register},
+  {path: 'dashboard', component: Dashboard},
+
+  // Redireciona para a página de login se a rota não for encontrada
+  {path: '', redirectTo: '/home', pathMatch: 'full'}
 ];
