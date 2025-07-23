a852c4ece88d658576529fcd935f9290b089a517
raunerlucas <raunerlucas@gmail.com>
2025-07-23 19:33:56 -0300

    Feat: Integrate Navbar component into Login page; update imports in login.ts

--- Frontend/gestao-projetos/src/app/pages/login/login.ts
+++ Frontend/gestao-projetos/src/app/pages/login/login.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import {Navbar} from '../../shared/navbar/navbar';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [
+    Navbar
+  ],
   templateUrl: './login.html',
   styleUrl: './login.css'
 })
