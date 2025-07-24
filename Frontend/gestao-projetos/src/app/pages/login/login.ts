b791da2d4013f387fdb9f7b5bb06a702efaf0b8e
raunerlucas <raunerlucas@gmail.com>
2025-07-23 21:17:25 -0300

    Feat: Create Register page with form fields and styling; update RouterLink for navigation

--- Frontend/gestao-projetos/src/app/pages/login/login.ts
+++ Frontend/gestao-projetos/src/app/pages/login/login.ts
@@ -1,10 +1,12 @@
 import {Component} from '@angular/core';
 import {Navbar} from '../../shared/navbar/navbar';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-login',
   imports: [
-    Navbar
+    Navbar,
+    RouterLink
   ],
   templateUrl: './login.html',
   styleUrl: './login.css'
--- Frontend/gestao-projetos/src/app/pages/register/register.ts
+++ Frontend/gestao-projetos/src/app/pages/register/register.ts
@@ -1,10 +1,12 @@
 import {Component} from '@angular/core';
 import {Navbar} from '../../shared/navbar/navbar';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-register',
   imports: [
-    Navbar
+    Navbar,
+    RouterLink
   ],
   templateUrl: './register.html',
   styleUrl: './register.css'
