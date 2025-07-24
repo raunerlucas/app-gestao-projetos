851d1ca5c97ef0eac6369c806d46ac3396b760b9
raunerlucas <raunerlucas@gmail.com>
2025-07-23 22:16:23 -0300

    Feat: Enhance login functionality with form validation and token management; update login component and template

--- Frontend/gestao-projetos/src/app/core/services/auth.ts
+++ Frontend/gestao-projetos/src/app/core/services/auth.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {environment} from '../../../environments/environment';
 import {HttpClient} from '@angular/common/http';
 
@@ -6,14 +6,35 @@ import {HttpClient} from '@angular/common/http';
   providedIn: 'root'
 })
 export class Auth {
+
   constructor(private http: HttpClient) {
   }
 
-  login(username: string, password: string) {
-    return this.http.post(`${environment.apiUrl}/auth/login`, { username, password });
+   login(username: string, password: string): boolean {
+    const userToken = this.http?.post(`${environment.apiUrl}/auth/login`, {username, password})
+      .subscribe({
+        next: (response: any) => {
+          console.log("Token received:", response.token);
+          localStorage.setItem('userToken', response.token);
+          return true;
+        },
+        error: (error) => {
+          console.error('Login failed', error);
+          return false;
+        }
+      });
+    return !!userToken;
   }
 
   logout() {
-    return this.http.post(`${environment.apiUrl}/auth/logout`, {});
+    return this.http.post(`${environment.apiUrl}/auth/logout`, {}).subscribe(
+      () => {
+        localStorage.removeItem('userToken');
+        console.log('Logout successful');
+      },
+      (error) => {
+        console.error('Logout failed', error);
+      }
+    );
   }
 }
