f9733c11d418496624cd3fff871714daa6417c44
raunerlucas <raunerlucas@gmail.com>
2025-08-21 15:34:54 -0300

    Feat: Update Pessoa model and request interfaces for required fields and improved structure

--- Frontend/gestao-projetos/src/app/models/Pessoa.model.ts
+++ Frontend/gestao-projetos/src/app/models/Pessoa.model.ts
@@ -4,32 +4,28 @@ export enum TipoPessoa {
 }
 
 export interface Pessoa {
-  id?: number;
+  id: number;
   nome: string;
   cpf: string;
-  email: string;
   telefone: string;
+  email: string;
   tipo: TipoPessoa;
-  dataCadastro?: Date;
-  disponivel?: boolean; // Apenas para avaliadores
   projetosIds?: number[]; // Apenas para autores
 }
 
 export interface PessoaCreateRequest {
   nome: string;
   cpf: string;
-  email: string;
   telefone: string;
+  email: string;
   tipo: TipoPessoa;
-  disponivel?: boolean;
   projetosIds?: number[];
 }
 
 export interface PessoaUpdateRequest {
   nome?: string;
   cpf?: string;
-  email?: string;
   telefone?: string;
-  disponivel?: boolean;
+  email?: string;
   projetosIds?: number[];
 }
